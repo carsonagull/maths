67527140f07e6959b214fd4e4986b72f36db0d84
John Carson <120414290+carsonagull@users.noreply.github.com>
2025-02-04 11:57:09 +0300

    Add files via upload

---
+++ maths.py
@@ -0,0 +1,4 @@
+import math
+radius= float(input("enter the radius of the sphere:"))
+volume= (4/3) *math.pi *(radius **3)
+print(f"the volume of the sphere with radius {radius} is: {volume:.2f} cubic units")
